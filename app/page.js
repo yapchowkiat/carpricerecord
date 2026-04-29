37efd3b61fd96f1d6273b70d344ba41c0ff711cc
yapchowkiat <chowkiat123@gmail.com>
2026-04-29 16:20:25 +0800

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,16 @@
+{
+     "name": "car-tracker",
+     "version": "1.0.0",
+     "private": true,
+     "scripts": {
+       "dev": "next dev",
+       "build": "next build",
+       "start": "next start"
+     },
+     "dependencies": {
+       "react": "^18",
+       "react-dom": "^18",
+       "next": "^14",
+       "lucide-react": "^0.383.0"
+     }
+   }
